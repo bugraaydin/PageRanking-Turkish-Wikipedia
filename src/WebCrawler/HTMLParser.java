6829c4d88fc7ed5605053e280e891cb4303025cf
bugraaydin <bugraaydin.cs@gmail.com>
2018-11-27 01:52:12 +0300

    basic bfs queue crawler with some data spanned from page "Ajdar"

--- src/WebCrawler/HTMLParser.java
+++ src/WebCrawler/HTMLParser.java
@@ -7,69 +7,57 @@ package WebCrawler;
         import org.jsoup.nodes.Element;
         import org.jsoup.select.Elements;
 
-        import java.io.File;
-        import java.io.FileWriter;
-        import java.io.IOException;
-        import java.io.PrintWriter;
-        import java.util.ArrayList;
-        import java.util.Scanner;
+        import java.io.*;
+        import java.util.ArrayDeque;
+        import java.util.Queue;
 
-        import static java.lang.Thread.sleep;
 
 public class HTMLParser{
+    static Queue<String> pagesToProcess = new ArrayDeque<String>();
     static int threadCount = 1;
     static final int MAX_THREAD = 5;
     public static final String path = "data/pages/";
     public static final String wikipediaDefault = "https://tr.wikipedia.org/wiki/";
     public static void main(String[] args) throws Exception{
-        String pageToParse = "Ajdar";
-        parsePage(pageToParse);
-        pageToParse = "Savur";
-        parsePage(pageToParse);
-        pageToParse = "Mardin";
-        parsePage(pageToParse);
-        pageToParse = "Türkiye";
-        parsePage(pageToParse);
-        pageToParse = "Pop";
-        parsePage(pageToParse);
-        pageToParse = "Şarkıcı";
-        parsePage(pageToParse);
-        pageToParse = "Besteci";
-        parsePage(pageToParse);
-        pageToParse = "Erol Büyükburç";
-        parsePage(pageToParse);
-        pageToParse = "Flash TV";
-        parsePage(pageToParse);
-        pageToParse = "Gülben Ergen";
-        parsePage(pageToParse);
-
+        pagesToProcess.add("Ajdar");
+        String pageToProcess = "";
+        while(pagesToProcess.peek() != null) {
+            parsePage(pagesToProcess.poll());
+        }
 
     }
-    public static void parsePage(String pageName) throws Exception{
+    public static void parsePage(String pageName){
         JSONObject json = new JSONObject();
         JSONArray pageNames = new JSONArray();
         JSONArray pageUrls = new JSONArray();
         String url = wikipediaDefault + pageName;
         System.out.println("Fetching %s..." + url);
-        Document doc = Jsoup.connect(url).get();
-        Elements links = doc.select("div#bodyContent");
-        links = doc.select("a[href^=/wiki]");
-        for (Element link : links) {
-            if(link.text() != "") {
-                pageNames.put(link.text());
-                //pageUrls.put(link.attr("href"));
-                pageUrls.put(java.net.URLDecoder.decode(link.attr("href"), "UTF-8"));
+        try {
+            Document doc = Jsoup.connect(url).get();
+            Elements links = doc.select("div#bodyContent");
+            links = doc.select("a[href^=/wiki]");
+            for (Element link : links) {
+                if(link.text() != "") {
+                    pageNames.put(link.text());
+                    pagesToProcess.add(link.text());
+                    //pageUrls.put(link.attr("href"));
+                    pageUrls.put(java.net.URLDecoder.decode(link.attr("href"), "UTF-8"));
+                }
+            }
+            json.put("Names", pageNames);
+            json.put("URLS", pageUrls);
+            try(FileWriter file = new FileWriter("data/pages/" +pageName + ".json")){
+                file.write(json.toString());
+                file.flush();
+                file.close();
+            }catch(IOException e){
+                e.printStackTrace();
             }
         }
-        json.put("Names", pageNames);
-        json.put("URLS", pageUrls);
-        try(FileWriter file = new FileWriter("data/pages/" +pageName + ".json")){
-            file.write(json.toString());
-            file.flush();
-            file.close();
-        }catch(IOException e){
-            e.printStackTrace();
+        catch(Exception e){
+            System.err.println("Failed crawling page: " + url );
         }
+
     }
     synchronized static void incrementThreadCount(){
         threadCount++;
