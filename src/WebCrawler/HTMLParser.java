b925784e35dc3335ffbbacaf0e7bff603c996701
bugraaydin <bugraaydin.cs@gmail.com>
2018-11-28 02:57:10 +0300

    updated more efficient algorithm

--- src/WebCrawler/HTMLParser.java
+++ src/WebCrawler/HTMLParser.java
@@ -8,44 +8,74 @@ package WebCrawler;
         import org.jsoup.select.Elements;
 
         import java.io.*;
-        import java.util.ArrayDeque;
-        import java.util.Queue;
+        import java.util.*;
 
 
 public class HTMLParser{
+    static Set<String> uselessStrings = new HashSet<String>();
+    static Set<String> alreadyParsedPages = new HashSet<String>();
     static Queue<String> pagesToProcess = new ArrayDeque<String>();
-    static int threadCount = 1;
-    static final int MAX_THREAD = 5;
-    public static final String path = "data/pages/";
+
+    public static Locale trlocale= new Locale("tr", "TR");
     public static final String wikipediaDefault = "https://tr.wikipedia.org/wiki/";
     public static void main(String[] args) throws Exception{
-        pagesToProcess.add("Ajdar");
-        String pageToProcess = "";
-        while(pagesToProcess.peek() != null) {
-            parsePage(pagesToProcess.poll());
+        ArrayList<String> useless = new ArrayList<String>();
+        useless.add("mesaj"); useless.add(""); useless.add(" "); useless.add("sorumluluk reddi");
+        useless.add("son değişiklikler"); useless.add("topluluk portali"); useless.add("yardım");
+        useless.add("sayfaya bağlantılar"); useless.add("anasayfa"); useless.add("ilgili değişiklikler");
+        useless.add("özel sayfalar"); useless.add("vikipedi hakkında"); useless.add("kaynak belirtilmeli");
+        useless.add("kaynak gösterme şablonları"); useless.add("daha iyi kaynak gerekli"); useless.add("tartışma");
+        useless.add("katkılar"); useless.add("oku"); useless.add("hakkımızda"); useless.add("içindekiler");
+        useless.add("köy çeşmesi"); useless.add("iş birliği projesi"); useless.add("deneme tahtası"); useless.add("seçkin içerik"); useless.add("rastgele madde");
+        useless.add("madde"); useless.add("ısbn sihirli bağlantısını kullanan sayfalar"); useless.add("kaynaksız anlatımlar içeren maddeler"); useless.add("bazı başlıkları geliştirilmeye ihtiyaç duyulan maddeler");
+        updateUselessList(useless);
+        queuePage("ajdar");
+
+        dequeuePage();
+        for(int i = 0; i < 500; i++) {
+            Thread newJob = new Thread() {
+                public void run() {
+                    try {
+                        while (!isEmpty()) {
+                            dequeuePage();
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            newJob.start();
         }
 
     }
+    static void updateUselessList(ArrayList<String> dictionary){
+        for(String str : dictionary){
+            uselessStrings.add(str);
+        }
+    }
     public static void parsePage(String pageName){
-        JSONObject json = new JSONObject();
-        JSONArray pageNames = new JSONArray();
-        JSONArray pageUrls = new JSONArray();
         String url = wikipediaDefault + pageName;
         System.out.println("Fetching %s..." + url);
         try {
+            if(!alreadyParsedPages.add(pageName)){
+                return;
+            }
+            if(uselessStrings.contains(pageName)){
+                return;
+            }
             Document doc = Jsoup.connect(url).get();
+            JSONObject json = new JSONObject();
+            JSONArray pageNames = new JSONArray();
             Elements links = doc.select("div#bodyContent");
-            links = doc.select("a[href^=/wiki]");
+            links = links.select("a[href^=/wiki]");
             for (Element link : links) {
-                if(link.text() != "") {
-                    pageNames.put(link.text());
-                    pagesToProcess.add(link.text());
+                if(!(uselessStrings.contains(link.text().toLowerCase(trlocale))) && !link.text().contains("(anlam ayrımı)") && !link.text().equals("") && !link.attr("href").contains("svg")) {
+                    pageNames.put(link.text().toLowerCase(trlocale));
+                    pagesToProcess.add(link.text().toLowerCase(trlocale));
                     //pageUrls.put(link.attr("href"));
-                    pageUrls.put(java.net.URLDecoder.decode(link.attr("href"), "UTF-8"));
                 }
             }
-            json.put("Names", pageNames);
-            json.put("URLS", pageUrls);
+            json.put("Links", pageNames);
             try(FileWriter file = new FileWriter("data/pages/" +pageName + ".json")){
                 file.write(json.toString());
                 file.flush();
@@ -59,25 +89,15 @@ public class HTMLParser{
         }
 
     }
-    synchronized static void incrementThreadCount(){
-        threadCount++;
+    synchronized static void queuePage(String str){
+        pagesToProcess.add(str);
     }
-    synchronized static void decrementThreadCount(){
-        threadCount--;
+    synchronized static void dequeuePage(){
+        parsePage(pagesToProcess.poll());
+    }
+    synchronized static boolean isEmpty(){
+        if(pagesToProcess.peek() != null)
+            return false;
+        return true;
     }
 }
-
-
-/*
-            Thread newJob = new Thread() {
-                public void run() {
-                    try {
-                        parsePage(link.text());
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            };
-                newJob.start();
-                incrementThreadCount();
- */
